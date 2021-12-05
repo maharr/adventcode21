0dbff90c8c1afee5b7945e56a25616d2b025f243
Matt Harrison <matt.harrison91@gmail.com>
2021-12-05 12:46:37 +0000

    q3 complete

--- 3/3.py
+++ 3/3.py
@@ -1,4 +1,4 @@
-with open("3/test.txt", "r") as f:
+with open("3/input.txt", "r") as f:
     input = [ x for x in f.read().splitlines()]
 
 count = [0] * len(input[0])
@@ -24,4 +24,61 @@ epsilon = gamma ^ ((2 ** len(input[0]))-1)
 
 print("Power is =", gamma * epsilon)
 
+filter =""
+oldlist = input
+for j in range(len(input[0])):
+    newlist = []
+    count = 0
+    for i in oldlist:
+        if i[j] == "0":
+            count -= 1
+        else:
+            count += 1
+    if count <0:
+        filter += "0"
+    else:
+        filter += "1"
 
+
+    newlist = [i for x,i in enumerate(oldlist) if i.startswith(filter)]
+    if len(newlist) != 1:
+        oldlist = newlist
+    else:
+        filter = newlist[0]
+        break
+
+    
+
+
+oxygenrate = int(filter,2)  
+print("Oxygen rate", oxygenrate)
+
+
+filter =""
+oldlist = input
+for j in range(len(input[0])):
+    newlist = []
+    count = 0
+    for i in oldlist:
+        if i[j] == "0":
+            count -= 1
+        else:
+            count += 1
+    if count >=0:
+        filter += "0"
+    else:
+        filter += "1"
+
+
+    newlist = [i for x,i in enumerate(oldlist) if i.startswith(filter)]
+    if len(newlist) != 1:
+        oldlist = newlist
+    else:
+        filter = newlist[0]
+        break
+
+
+co2rate = int(filter,2)
+print("CO2 rate", co2rate)
+
+print("Support rating", co2rate * oxygenrate)
