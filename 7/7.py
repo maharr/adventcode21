18d5e3f35ad1eed86f13d6cd5779e9a4357583c8
Matt Harrison <matt.harrison91@gmail.com>
2021-12-11 22:28:06 +0000

    q7 complete

--- 7/7.py
+++ 7/7.py
@@ -1,23 +1,29 @@
 import numpy as np
-from numpy.core.fromnumeric import repeat
 def calcFuel(start):
     return abs(start[0] - start[1])
 
-from numpy.lib.function_base import median
+def calcFuelTri(start):
+    dist = abs(start[0] - start[1])
+    return ((dist**2)+dist)/2
+
 with open("7/input.txt", "r") as f:
     numbers = [ int(x) for x in f.readline().split(",") ]
 
 dest = np.median(numbers)
 
 current = sum(map(calcFuel, [(x,dest) for x in numbers]))
+print("First fuel usage ", int(current))
+current = sum(map(calcFuelTri, [(x,dest) for x in numbers]))
 
 while True:
-    low = sum(map(calcFuel, [(x,dest-1) for x in numbers]))
-    high = sum(map(calcFuel, [(x,dest+1) for x in numbers]))
+    low = sum(map(calcFuelTri, [(x,dest-1) for x in numbers]))
+    current = sum(map(calcFuelTri, [(x,dest) for x in numbers]))
+    high = sum(map(calcFuelTri, [(x,dest+1) for x in numbers]))
     if low > current < high:
-        print(int(current))
         break
     elif low < current:
-        dest += 1
-    elif high < current:
         dest -= 1
+    elif high < current:
+        dest += 1
+
+print("Second fuel usage ", int(current))
