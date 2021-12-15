2640fc30135b89573a929827131ee740d2bce8a0
Matt Harrison <matt.harrison91@gmail.com>
2021-12-15 10:49:09 +0000

    q8 part 1 complete

--- 8/8.py
+++ 8/8.py
@@ -1,4 +1,4 @@
-with open("8/input.txt", "r") as f:
+with open("8/test.txt", "r") as f:
     lines = [x.split() for x in f.readlines()]
 
 count = 0
@@ -13,6 +13,26 @@ for line in lines:
             count += 1
         elif len(line[i]) == 7: # number 8
             count += 1
+        else: # number 0, 2, 3, 5, 6, 9
+            pass
 
 print("Total ", count)
+
+
+
+for line in lines:
+    seg = ["abcdefg"] * 7
+    for i in range(0,10):
+        print(seg)
+        if len(line[i]) == 2: # number 1
+            seg[3] = line[i]
+            seg[6] = line[i]
+        elif len(line[i]) == 4: # number 4
+            pass
+        elif len(line[i]) == 3: # number 7
+            pass
+        elif len(line[i]) == 7: # number 8
+            pass
+        else: # number 0, 2, 3, 5, 6, 9
+            pass
 
