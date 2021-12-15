b873af474d4e44305012fe5d50dbd8298ae168be
Matt Harrison <matt.harrison91@gmail.com>
2021-12-15 20:45:37 +0000

    q10 complete

--- 10/10.py
+++ 10/10.py
@@ -9,10 +9,12 @@ pairs = {
     "<": ">"
 }
 points = 0
-
+completed = []
 
 for line in lines:
     stack = []
+    complete = 0
+    corrupted = False
     for char in line:
         if char in opens:
             stack.append(char)
@@ -20,6 +22,7 @@ for line in lines:
             stack.pop()
         else:
             print("Error, expected ", pairs[stack[-1]], " but instead found ", char)
+            corrupted = True
             if char == ")":
                 points += 3
             elif char == "]":
@@ -29,5 +32,19 @@ for line in lines:
             elif char == ">":
                 points += 25137
             break
+    if not corrupted:
+        for i in range(len(stack),0, -1):
+            val = stack.pop()
+            complete *= 5
+            if val == "(":
+                complete += 1
+            elif val == "[":
+                complete += 2
+            elif val == "{":
+                complete += 3
+            elif val == "<":
+                complete += 4
+        completed.append(complete)
 
 print("Total points ", points)
+print("Total completion points ", sorted(completed)[len(completed)//2])
