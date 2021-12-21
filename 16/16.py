cb3ebaecfd4b654b2d9567c19a5a22bf9a65e2f0
Matt Harrison <matt.harrison91@gmail.com>
2021-12-21 09:56:45 +0000

    q16 in progress

--- 16/16.py
+++ 16/16.py
@@ -1,3 +1,6 @@
+from re import sub
+
+
 with open("16/test.txt", "r") as f:
     lines = f.read().splitlines()
 
@@ -5,4 +8,19 @@ for line in lines:
     number = int(line,16)
     packet = str(bin(number))
     print(packet)
+    version = int(packet[2:5],2)
+    id = int(packet[5:8],2)
+    message = packet[8:]
+    if id == 4:
+        sub_packet = ""
+        for i in range(len(message)//5):
+            start = (i * 5) + 1
+            end = (i * 5) + 5
+            sub_packet += message[start:end]
+        print(int(sub_packet,2))
+        
+    print(version,id)
+    print(sub_packet)
+
+
     
