bd2d2ba76b01bf49006d0dcf9fc8041659cdad89
Matt Harrison <matt.harrison91@gmail.com>
2022-08-15 18:34:24 +0100

    q25 update

--- 25/25.py
+++ 25/25.py
@@ -1,10 +1,62 @@
-with open("25/test1.txt", "r") as f:
+with open("25/test.txt", "r") as f:
     lines = [ list(x) for x in f.read().splitlines()]
 
-print(lines)
-for line in lines:
-    for i,col in enumerate(line):
-        if col == ">" and line[(i+1) % len(line) ] ==".":
-            col = "."
-            line[(i+1) % len(line)] = ">"
-print(lines)
+for row in lines:
+    print(*row, sep="\t")
+
+print(len(lines[0]))
+
+counter = 0
+
+while True:
+    counter += 1
+    change = False
+    new_grid = [["." for x in range(len(lines[0]))] for y in range(len(lines))]
+
+    for i,line in enumerate(lines):
+        for j,col in enumerate(line):
+            next_col = (j+1) % len(line)
+
+            if col == ">":
+                if line[next_col] == ".":
+                    new_grid[i][next_col] = ">"
+                else:
+                    new_grid[i][j] = ">"
+            
+            if col == "v":
+                new_grid[i][j] = "v"
+
+    if lines == new_grid:
+        change = True
+
+    lines = new_grid.copy()
+
+    new_grid = [["." for x in range(len(lines[0]))] for y in range(len(lines))]
+
+    for i,line in enumerate(lines):
+        for j,col in enumerate(line):
+            next_row = (i+1) % len(lines)
+
+            if col == 'v':
+                if lines[next_row][j] == ".":
+                    new_grid[next_row][j] = "v"
+                else:
+                    new_grid[i][j] = "v"
+            
+            if col == ">":
+                new_grid[i][j] = ">"
+
+    if lines == new_grid:
+        change = True
+
+    lines = new_grid.copy()
+
+    for row in new_grid:
+        print(*row, sep="\t")
+
+    if not change:
+        False
+    
+    input("Press enter for next iteration " + str(counter))
+    
+
