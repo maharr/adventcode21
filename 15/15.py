3aa6b095724e477a0a71ebd5e2538f1535e7f6f7
Matt Harrison <matt.harrison91@gmail.com>
2022-08-22 11:19:18 +0100

    add timing to q15

--- 15/15.py
+++ 15/15.py
@@ -1,9 +1,11 @@
 from heapq import heappop, heappush
+import time
 
 with open("15/input.txt", "r") as f:
     grid = [[int(y) for y in list(x)] for x in f.read().splitlines()]
 
 height, width = len(grid), len(grid[0])
+start = time.perf_counter()
 
 for i in 1,5:
     open_nodes = [(0,0,0)]
@@ -14,6 +16,7 @@ for i in 1,5:
         current_distance = current_node[0]
         if current_node[1] == i * height-1 and current_node[2] == i * width-1:
             print(current_distance)
+            print(f"Processing time for Q{i}, {time.perf_counter() - start:0.4f} seconds")
             break
         
         for d in directions:
